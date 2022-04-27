93a9b2975b7561e53199d47ad1a122739f151d23
Ashok Krishnamurthy <ashokkrish@gmail.com>
2022-04-27 12:46:27 -0600

    Add files via upload

---
+++ parmsValidate.R
@@ -0,0 +1,39 @@
+#install.packages("shiny")
+#install.packages("shinyvalidate")
+#install.packages("shinyWidgets")
+
+library(shiny)
+library(shinyvalidate)
+#library(shinyWidgets)
+
+ui <- fluidPage(
+  titlePanel("Epidemic Parameters"),
+
+  sidebarLayout(
+    sidebarPanel(
+
+      numericInput(inputId = "beta",
+                   label = "Daily Exposure Rate", 
+                   value = 0.05, min = 0, max = 1, step = 0.00001),
+      
+      numericInput(inputId = "timestep",
+                   label = "Number of Iterations (days)",
+                   min = 1, max = 3650, value = 3, step = 1),
+      
+      actionButton(inputId = "validate", label = "Validate")
+    ),
+    
+    mainPanel(
+
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  iv <- InputValidator$new()
+  iv$add_rule("beta", sv_required())
+  iv$add_rule("timestep", sv_required())
+  iv$enable()
+}
+
+shinyApp(ui, server)
